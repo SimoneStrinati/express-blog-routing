b07c97db02bdac4d806567c6cae6bd98d75a4767
simonestrinati <simone.strinati@outlook.it>
2026-02-17 20:46:04 +0100

    istanza express

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const postsRouter = require("./routers/posts")
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use("/posts/", postsRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
