53b89ca8e8598ea9d3af99717602934960279473
simonestrinati <simone.strinati@outlook.it>
2026-02-25 19:45:59 +0100

    bonus export controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,87 +1,83 @@
 const posts = require("../data/posts")
 
-function index(req, res) {
-
-    let risultati = posts
-
-    if (req.query.tags) {
-
-        risultati = posts.filter(post => post.tags.find(tag => tag.toLowerCase() === req.query.tags.toLowerCase()));
-    }
-
-    res.json(risultati);
-};
-
-
-function show(req, res) {
-    const id = Number(req.params.id);
-
-    const risultato = posts.find(post => post.id == id)
-
-    if (!risultato) {
-        res.status(404).res.json({ error: "Not Found", message: "Post non trovato" });
-    }
-
-    res.json(risultato);
-};
-
-function store(req, res) {
-    // console.log(req.body);
-
-    const newId = Math.max(0, ...posts.map(post => post.id)) + 1;
-
-    const newPost = {
-        id: newId, //l'ID non dobbiamo scriverlo noi in realtà, perché sarà il database a crearlo per noi.
-        title: req.body.title,
-        content: req.body.content,
-        image: req.body.image,
-        tags: req.body.tags
-    };
-
-    posts.push(newPost);
-
-    res.status(201).json(newPost)
-};
-
-function update(req, res) {
-
-    const id = Number(req.params.id);
-
-    const risultato = posts.find(post => post.id == id)
-
-    if (!risultato) {
-        res.status(404).res.json({ error: "Not Found", message: "Post non trovato" });
-    }
-
-    risultato.title = req.body.title;
-    risultato.content = req.body.content;
-    risultato.image = req.body.image;
-    risultato.tags = req.body.tags
-
-    res.json(risultato);
-    
-    
-};
-
-function destroy(req, res) {
-
-    const id = Number(req.params.id);
-
-    const risultato = posts.find(post => post.id == id);
-
-    posts.splice(posts.indexOf(risultato), 1);
-
-    console.log(`Hai eliminato il post: ${id}`, posts);
-
-    res.sendStatus(204);
-};
 
 const controllers = {
-    index,
-    show,
-    store,
-    update,
-    destroy
+
+    index: function (req, res) {
+
+        let risultati = posts
+
+        if (req.query.tags) {
+
+            risultati = posts.filter(post => post.tags.find(tag => tag.toLowerCase() === req.query.tags.toLowerCase()));
+        }
+
+        res.json(risultati);
+    },
+
+    show: function (req, res) {
+        const id = Number(req.params.id);
+
+        const risultato = posts.find(post => post.id == id)
+
+        if (!risultato) {
+            res.status(404).res.json({ error: "Not Found", message: "Post non trovato" });
+        }
+
+        res.json(risultato);
+    },
+
+    store: function (req, res) {
+        // console.log(req.body);
+
+        const newId = Math.max(0, ...posts.map(post => post.id)) + 1;
+
+        const newPost = {
+            id: newId, //l'ID non dobbiamo scriverlo noi in realtà, perché sarà il database a crearlo per noi.
+            title: req.body.title,
+            content: req.body.content,
+            image: req.body.image,
+            tags: req.body.tags
+        };
+
+        posts.push(newPost);
+
+        res.status(201).json(newPost)
+    },
+
+    update: function (req, res) {
+
+        const id = Number(req.params.id);
+
+        const risultato = posts.find(post => post.id == id)
+
+        if (!risultato) {
+            res.status(404).res.json({ error: "Not Found", message: "Post non trovato" });
+        }
+
+        risultato.title = req.body.title;
+        risultato.content = req.body.content;
+        risultato.image = req.body.image;
+        risultato.tags = req.body.tags
+
+        res.json(risultato);
+
+
+    },
+
+    destroy: function (req, res) {
+
+        const id = Number(req.params.id);
+
+        const risultato = posts.find(post => post.id == id);
+
+        posts.splice(posts.indexOf(risultato), 1);
+
+        console.log(`Hai eliminato il post: ${id}`, posts);
+
+        res.sendStatus(204);
+    }
+
 };
 
 module.exports = controllers;
